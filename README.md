5300f3f4ba60638b749f5d653f17aba4fd8d0e09
annmunyare <annmunyare28@yahoo.com>
2016-11-29 12:49:55 +0300

    more user inteface logic

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Author: Ann Munyare
 Specs: The program checks if the year inputted by the user if it is divisible by 4 and returns a  boolean.
+also checks if it is divisible by 100 and 400.
--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,22 @@ var leapYear = function(year) {
         return false;
     }
 };
+
+// user interface logic
+$(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+        event.preventDefault();
+        var year = parseInt($("input#year").val());
+        var result = leapYear(year);
+
+        $(".year").text(year);
+
+        if (!result) { // same as writing if (result === false)
+            $(".not").text("not");
+        } else {
+            $(".not").text("");
+        }
+
+        $("#result").show();
+    });
+});
